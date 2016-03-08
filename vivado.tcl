58a0d4955cdc4e8d6dbde909aa773ad00ef226a0
Emily Ng <ngemily2@gmail.com>
2016-03-08 10:15:59 -0500

    fix to open netlist before writing

--- vivado.tcl
+++ vivado.tcl
@@ -11,10 +11,12 @@ synth_design -rtl -name rtl_1
 
 launch_runs synth_1
 wait_on_run synth_1
+open_run synth_1 -name netlist_1
 
 write_verilog -mode funcsim $outDir/top_synth.vo -force
 
 launch_runs impl_1
 wait_on_run impl_1
+open_run impl_1 -name netlist_2
 
 write_verilog -mode funcsim $outDir/top_impl.vo -force
